e1f8641f037427e14a5bee0190eb8cb741cf2023
hamzaalkharouf <64504487+hamzaalkharouf@users.noreply.github.com>
2020-09-28 21:39:42 +0300

    save data in csv

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request
 import scikit_learn
+import Write_Csv
 app = Flask(__name__)
 #append data(from url) to list
 def Data_append(x1,x2,x3,x4,x5,x6):
@@ -14,7 +15,7 @@ def Data_append(x1,x2,x3,x4,x5,x6):
 
 #route /
 #take data from url then send them to scikit_learn of Calculate price from scikit
-#return information 
+#return information
 @app.route('/')
 def hello_world():
         transaction_date=float(request.args.get('transaction_date'))
@@ -26,6 +27,8 @@ def hello_world():
         list_data=[]
         list_data=Data_append(transaction_date,house_age,distance_to_the__nearest_MRT_station,number_of_convenience_stores,latitude,longitude)
         price=scikit_learn.path(list_data)
+        list_data.append(price)
+        Write_Csv.Write_Csv(list_data)
         return '''<h3>
         transaction date : {}<br>
         house age= {}<br>
@@ -36,7 +39,7 @@ def hello_world():
         price ={}
         </h3>'''.format(transaction_date,house_age,distance_to_the__nearest_MRT_station,number_of_convenience_stores,latitude,longitude,price)
 
-#to run servier => py app.py -i ./model.pickle
+#to run servier => py app.py -path ./model.pickle
 if __name__ == '__main__':
     app.run(port=5060,debug=False,use_reloader=False)
 # http://127.0.0.1:5060/?transaction_date=2017.917&house_age=10&distance_to_the__nearest_MRT_station=306.59470&number_of_convenience_stores=15&latitude=24.98034&longitude=121.53951
